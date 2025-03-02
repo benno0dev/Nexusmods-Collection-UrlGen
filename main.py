963d6baef8f1cd1afadcb5e2e47bf1b771ace2c9
Benno <81915723+benno0dev@users.noreply.github.com>
2025-03-02 15:51:07 +0100

    updated main.py
    changed the way the urls get generated and added options to open the default description page or download the file in the collection (download over browser / download in vortex)

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 ### CONFIG
-TXT_MODE = False # If it should just write the urls or if to put a 'start' infront of them to start them in your browser
-FILE_TYPE = 'bat' # Usual file types: 'sh', 'bat', 'txt'
-GAME_NAME = 'cyberpunk2077' # check nexusmods for the right spelling
+TXT_MODE = False
+# If it should just write the urls or if to put a 'start' infront of them to start them in your browser
+
+FILE_TYPE = 'bat'
+# Usual file types: 'sh', 'bat', 'txt'
+
+GAME_NAME = 'cyberpunk2077'
+# check nexusmods for the right spelling
+
+DOWNLOAD_MODE = 'vortex'
+# 'vortex': opens download page for vortex mod manager
+# 'normal': opens download page for normal download (zip file)
+# 'file': opens only the files tab
+# 'none': doesn't open download page / opens the description page
 
 ### CODE
 import json
@@ -11,13 +22,22 @@ with open('collection.json', 'r') as file:
 
 mods = data["mods"]
 
+with open(f'collection.{FILE_TYPE}', "w") as file:
+    file.write('')
+
 bat = open(f"collection.{FILE_TYPE}", "a")
 for mods in mods:
     source = mods["source"]
     modId = source["modId"]
-    if not TXT_MODE:
-        url = "start " + f"https://www.nexusmods.com/{GAME_NAME}/mods/" + str(modId) + "/?tab=files\n"
-    else:
-        url = f"https://www.nexusmods.com/{GAME_NAME}/mods/" + str(modId) + "/?tab=files\n"
+    fileId = source["fileId"]
+    if not TXT_MODE: start = 'start '
+    else: start = ''
+    match DOWNLOAD_MODE:
+        case 'vortex': download = f'?tab=files&file_id={fileId}&nmm=1'
+        case 'normal': download = f'?tab=files&file_id={fileId}'
+        case 'file': download = f'?tab=files'
+        case 'none' | '': download = ''
+        case _: raise IOError('No Download specified, reconfigurate to continue')
+    url = f"{start}https://www.nexusmods.com/{GAME_NAME}/mods/{str(modId)}/{download}\n"
     bat.write(url)
     
